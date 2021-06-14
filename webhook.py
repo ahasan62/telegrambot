ec100c3f3ebf8a0ce03234dfca46f00bbb47336a
Abbas Hasan <abbas@Abbass-MacBook-Air.local>
2021-06-14 08:28:08 +0400

    docker setup

--- webhook.py
+++ webhook.py
@@ -1,11 +1,11 @@
-import requests
-import json 
+# import requests
+# import json 
 
-# need a webhook destination: 
-webook_url = 'https://webhook.site/f3e693f5-8b6f-40b4-b197-1ef1888cb1fa'
+# # need a webhook destination: 
+# webook_url = 'https://webhook.site/f3e693f5-8b6f-40b4-b197-1ef1888cb1fa'
 
-data = {'name': "Telegram",
-        'Channel URL': 'https://www.youtube.com/watch?v=X-_25tzo8Cw' }
+# data = {'name': "Telegram",
+#         'Channel URL': 'https://www.youtube.com/watch?v=X-_25tzo8Cw' }
 
 
-r = requests.post(webook_url, data=json.dumps(data), headers={'Content-Type' : 'application/json'})
+# r = requests.post(webook_url, data=json.dumps(data), headers={'Content-Type' : 'application/json'})
